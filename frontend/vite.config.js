b090cef59994c8f57b4e67e8c183aa70bc61d5b4
Abangale <aldra.aziz@gmail.com>
2025-02-25 16:01:37 +0700

    added in vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,4 +7,9 @@ export default defineConfig({
   plugins: [vue(),
     tailwindcss(),
   ],
+  server: {
+    host: '0.0.0.0', // Mengizinkan akses dari jaringan
+    port: 5173, // Pastikan ini sesuai dengan yang digunakan di Docker
+    strictPort: true // Hindari perubahan port otomatis
+  },
 })
