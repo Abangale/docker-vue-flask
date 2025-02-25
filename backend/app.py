1c9cae0b83b8e22283e22b9d6764ea1f52574124
Abangale <aldra.aziz@gmail.com>
2025-02-25 13:26:46 +0700

    forgot bro

--- backend/app.py
+++ backend/app.py
@@ -7,4 +7,4 @@ def hello():
     return jsonify({"message": "Hello from Flask!"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
